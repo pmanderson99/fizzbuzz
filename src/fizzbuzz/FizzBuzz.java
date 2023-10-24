594e17f445f115108c7f2cbc042562312a14bde7
Paige Anderson <paige.m.anderson1999@gmail.com>
2023-10-23 22:48:41 -0700

    initial commit
    loop exercise

---
+++ src/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,21 @@
+package fizzbuzz;
+
+public class FizzBuzz {
+	
+	public static void main(String[] args) {
+		
+		int num = 100;
+		for(int i = 1; i <= num; i++) {
+			if(i % 5 == 0 && i % 3 == 0) {
+				System.out.println("FizzBuzz!");
+			} else if( i % 3 == 0) {
+				System.out.println("Fizz");
+			} else if(i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
